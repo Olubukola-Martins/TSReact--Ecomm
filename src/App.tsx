3e82952c225baebd92a0bcdba4d4e0ebfc1a3f9f
Olubukola <19bukola97@gmail.com>
2023-07-20 16:04:51 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
+
+function App() {
+
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={} />
+        <Route path="/" element={} />
+        <Route path="/" element={} />
+      </Routes>
+    </Container>
+  );
+
+}
+
+export default App
